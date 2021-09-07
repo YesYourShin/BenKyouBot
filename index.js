3cc7263cb708cbe626a45b14e398160b2478346f
YesYourShin <ehrl827@naver.com>
2021-09-07 18:13:39 +0900

    check join and leave voice channel

--- index.js
+++ index.js
@@ -9,20 +9,38 @@ app.on('ready', () => { // 여기서 사용되는 Arrow Function은 콜백함수
 });
 
 app.on('voiceStateUpdate', (oldState, newState) => {
-    const checkCh = newState.guild.channels.cache.get('883309516551233547');
-    const joinedAt = newState.guild.channels.joinedAt;
-    const joinedAt2 = newState.member.joinedAt;
-    const createdTimeStamp = newState.guild.createdTimeStamp;
+    const checkCh = newState.guild.channels.cache.get('884707409229148160');
+    const name = newState.member.user.username;
 
-    console.log('joinedAt : ' + joinedAt );
-    console.log('joinedAt : ' + joinedAt2 );
-    console.log('createdTimeStamp : ' + createdTimeStamp );
     // 유저가 음성 채널에 들어왔을 때 혹은 나갔을 때 실행
     if(oldState.channelID != newState.channelID) {
-        
+        const time = new Date();
+        const year = time.getFullYear();
+        const month = time.getMonth()+1;
+        const date = time.getDate();
+        const day = (time.getDay() == 0) ? "일요일" :
+                        (time.getDay() == 1) ? "월요일" :
+                        (time.getDay() == 2) ? "화요일" :
+                        (time.getDay() == 3) ? "수요일" :
+                        (time.getDay() == 4) ? "목요일" :
+                        (time.getDay() == 5) ? "금요일" : "토요일"
+        const hours = time.getHours();
+        const min = time.getMinutes();
+        const sec = time.getSeconds();
+        // console.log(now);
+
+
+        const now = year + "년 " + month + "월 " + date + "일 " + day + " " + hours + "시 " + min + "분 " + sec + "초"; 
+        if(newState.channelID) {
+            checkCh.send("예비역 병장 " + name + " 상번 시간 \n" + String(now));
+            console.log('userJoin');
+        }
+
         if(!newState.channelID) {
+            
+            checkCh.send("예비역 병장 " + name + " 하번 시간 \n" + String(now));
             console.log('userLeave');
-            checkCh.send('체크');
+            
         }
     }
 
